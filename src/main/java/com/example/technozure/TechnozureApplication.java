c9cac25585343f208f933ea21c879588bdd7f180
Jai Prakash <jaiprakash@Jais-MacBook-Air.local>
2023-03-22 23:02:03 +0800

    initial

---
+++ src/main/java/com/example/technozure/TechnozureApplication.java
@@ -0,0 +1,13 @@
+package com.example.technozure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnozureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnozureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/technozure/TechnozureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technozure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnozureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
